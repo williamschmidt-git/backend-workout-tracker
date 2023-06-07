ae3cabdf3e2224dee682e5aea8000f49275b13b1
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-07 17:32:20 -0300

    server created + eslint + ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.get('/', (request, response) => response.json({
-  message: 'hello world',
-}));
+class App {
+  public app: express.Application;
 
-app.listen(3000, () => {
-  console.log('rodou!!!!!!!!!!!!!!!!');
-});
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    // connectToDatabase();
+    this.app.use(express.json());
+    // this.app.use('/', userRouter);
+    // this.app.use('/', loginRouter);
+    // this.app.use('/category', categoryRouter);
+    // this.app.use('/product', productRouter);
+    // this.app.use(errorMiddleware);
+  }
+
+  public start(): void {
+    this.app.listen(process.env.PORT, () => {
+      console.warn(`Listening on port ${process.env.PORT}`);
+    });
+  }
+}
+
+export { App };
