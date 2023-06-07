f804e665abe3a14a3f328c33d5be4bcbb344de6b
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-07 17:26:07 -0300

    add lint ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => response.json({
-  message: 'hello world'
-}))
+  message: 'hello world',
+}));
 
 app.listen(3000, () => {
-  console.log('rodou!!!!!!!!!!!!!!!!')
-})
+  console.log('rodou!!!!!!!!!!!!!!!!');
+});
