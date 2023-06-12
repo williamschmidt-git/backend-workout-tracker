1d7497f303057dae3496e78bdbc96399240c12a1
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-11 21:56:22 -0300

    teste

--- src/routes/Login.ts
+++ src/routes/Login.ts
@@ -4,6 +4,8 @@ import { loginController } from '../factories/Login';
 
 const loginRouter = express.Router();
 
+// login route
+
 loginRouter.post('/', (req, res) => {
   loginController.login(req, res);
 });
