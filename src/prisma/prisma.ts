2733d1d6af4782b0cc8a58b75de7031531152f6f
williamschmidt-git <9williamschmidt@gmail.com>
2023-06-11 21:13:56 -0300

    removing some files

--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -13,17 +13,17 @@ async function main() {
   //   },
   // });
 
-  const squat = await prisma.workout.create({
-    data: {
-      workout_name: 'Assisted Squat',
-      workout_type: 'Squat',
-      how_to_perform: 'Stand up straight with legs shoulder widht apart | Grab something in front of you and use your hands to assist in the squat. Reduce assistance over time | Initiate the movement by sendint the hips back as if youre sitting back into an invisible chair | Go as low as you can while maintaining perfect form | Press through heels to stand back up to starting position',
-      form: 'Stand up straight at the top | Go as low as you can, preferably until the hips are below the knees | Dig your big toe and heel into the ground | Make sure you keep your heels down and keep them planted throughout the move | Keep your knee-in-line with your toes | Dont let the knees come inward on either the descend or the ascend; think about pushing the knees out | Keep lower back neutral. Not hollow, not arched',
-      example_url: 'https://youtu.be/OuR_Fp7AB0c',
-    },
-  });
+  // const squat = await prisma.workout.create({
+  //   data: {
+  //     workout_name: 'Assisted Squat',
+  //     workout_type: 'Squat',
+  //     how_to_perform: 'Stand up straight with legs shoulder widht apart | Grab something in front of you and use your hands to assist in the squat. Reduce assistance over time | Initiate the movement by sendint the hips back as if youre sitting back into an invisible chair | Go as low as you can while maintaining perfect form | Press through heels to stand back up to starting position',
+  //     form: 'Stand up straight at the top | Go as low as you can, preferably until the hips are below the knees | Dig your big toe and heel into the ground | Make sure you keep your heels down and keep them planted throughout the move | Keep your knee-in-line with your toes | Dont let the knees come inward on either the descend or the ascend; think about pushing the knees out | Keep lower back neutral. Not hollow, not arched',
+  //     example_url: 'https://youtu.be/OuR_Fp7AB0c',
+  //   },
+  // });
 
-  console.log(squat);
+  // console.log(squat);
 }
 
 main()
